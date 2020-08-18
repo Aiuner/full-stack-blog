54dda8f5b69101dc22245b1a1103203193e6dab6
Aiuner <demonspawndaemnyxea@gmail.com>
2020-08-18 14:07:50 -0400

    Routes set up in App.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PostList from './components/postList/PostList.jsx'
-import { Route } from 'react-router-dom'
-import Home from './screens/home/Home'
-
+import { Route, Switch } from 'react-router-dom';
+import Home from './screens/home/Home.jsx';
+import Create from './screens/create/Create.jsx';
+import Edit from './screens/edit/Edit.jsx';
+import PostView from './screens/post/PostView.jsx';
 
 function App() {
   return (
     <div>
-    <Route path='/'>
-        <Home />  
-      </Route>
-      
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/create-new-post' component={Create} />
+        <Route exact path='/posts/:id' component={PostView} />
+        <Route exact path='/posts/:id/edit' component={Edit} />
+      </Switch>
     </div>
   );
 }
